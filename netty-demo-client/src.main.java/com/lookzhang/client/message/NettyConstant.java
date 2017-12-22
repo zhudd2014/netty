23e5f2ea731aaf67ce381a0c0cab3eeb46708f5a
lookzhang <89255371@qq.com>
2017-12-22 16:06:29 +0800

    init

--- netty-demo-client/src.main.java/com/lookzhang/client/message/NettyConstant.java
+++
@@ -1,7 +0,0 @@
-package com.lookzhang.client.message;
-
-public class NettyConstant {
-    public static final String IP = "127.0.0.1";
-    public static final int SERVER_PORT = 9999;
-    public static final int CLIENT_PORT = 9998;
-}
